24751220f24e385720b29213886eca3bb276d19c
mixen56 <46136873+mixen56@users.noreply.github.com>
2022-04-07 18:15:02 +0300

    Update

--- run.sh
+++ run.sh
@@ -4,15 +4,16 @@ ITERS=100000
 
 # start
 run(){
-    ./generate_data $ITERS
-    ./check_deps
-    ./services_helper --start
-    ./cassandra_helper clean
-    ./cassandra_helper prepare
+    ./generate_data $ITERS              | tee full.log
+    ./check_deps 2>&1                   | tee -a full.log
+    ./services_helper --start 2>&1      | tee -a full.log
+    ./cassandra_helper clean   2>&1     | tee cassandra.log
+    ./cassandra_helper prepare 2>&1     | tee -a cassandra.log
     ./kafka_helper prepare
     ./kafka_helper upload
-    mvn package
-    java -jar target/LABA_2-1.0-SNAPSHOT-jar-with-dependencies.jar ./vocab.txt
+    mvn package 2>&1 | tee build.log
+    java -jar target/LABA_2-1.0-SNAPSHOT-jar-with-dependencies.jar ./vocab.txt 2>&1 \
+                                        | tee job.log
     echo "Enter: ./cassandra_helper show"
     exit 0
 }
